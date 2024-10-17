cbc075a2e9bef887c3512d2e464991e096af5f7e
Ghanshayam Kumar Giet <ghanshyam.kumar.edu@gmail.com>
2024-10-17 22:06:44 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import PagenotFound from "./pages/PagenotFound";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<PagenotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <p> contact name Ghanshyam kumar </p>
+      <p> Mob no 7281015610</p>
+      <p> email- gk728101@gmail.com</p>
+    </Layout>
+  );
+};
+
+export default Contact;
