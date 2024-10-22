bd76765f30be57cbd3ec7f9fb7e19953ea0e82bc
Ghanshayam Kumar Giet <ghanshyam.kumar.edu@gmail.com>
2024-10-22 07:07:48 +0530

    change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
-import { useAuth } from "../context/auth";
+// import { useAuth } from "../context/auth";
 
 const HomePage = () => {
   //const { auth } = useAuth(); // Destructure as an object
